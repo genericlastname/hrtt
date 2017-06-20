ad7899e5b6e085b147a7a62dc45491dde9616fae
Kyle Brown <brownkylej@gmail.com>
2017-06-20 10:27:23 -0500

    Use argparse instead of homebaked methods

--- hrtt/hrtt.py
+++ hrtt/hrtt.py
@@ -1,5 +1,5 @@
-import sys
 import os
+import argparse
 from .scrape import load_accounts
 from .scrape import scrape_from_user
 from .tweet import load_tweets
@@ -12,23 +12,48 @@ def cleanup():
 
 
 def main():
-    if len(sys.argv) < 2:
-        print('Not enough arguments!')
-    elif len(sys.argv) >= 2:
-        if sys.argv[1] == 'cleanup':
-            cleanup()
-        elif sys.argv[1] == 'scrape':
-            acc = load_accounts('data/accounts.txt')
-            for x in range(len(acc)):
-                print('Account {}'.format(x+1))
-                scrape_from_user(acc, x)
-        elif sys.argv[1] == 'save':
-            tweets = load_tweets('data/tweet_ids.txt')
-            print(tweets)
-            with open('data/tweets.pickle', 'wb+') as f:
-                pickle.dump(tweets, f)
-        elif sys.argv[1] == 'load':
-            with open('data/tweets.pickle') as f:
-                tweets = pickle.load(f)
-        else:
-            print('Not a recognized argument')
+    parser = argparse.ArgumentParser(description='Analyze Twitter data')
+    parser.add_argument('command', help='what subprogram hrtt should run')
+    args = parser.parse_args()
+
+    if args.command == 'clean':
+        cleanup()
+
+    elif args.command == 'scrape':
+        acc = load_accounts('data/accounts.txt')
+        for x in range(len(acc)):
+            print('Account {}'.format(x+1))
+            scrape_from_user(acc, x)
+
+    elif args.command == 'save':
+        tweets = load_tweets('data/tweet_ids.txt')
+        print(tweets)
+        with open('data/tweets.pickle', 'wb+') as f:
+            pickle.dump(tweets, f)
+
+    elif args.command == 'load':
+        with open('data/tweets.pickle') as f:
+            tweets = pickle.load(f)
+
+
+# def main():
+#     if len(sys.argv) < 2:
+#         print('Not enough arguments!')
+#     elif len(sys.argv) >= 2:
+#         if sys.argv[1] == 'cleanup':
+#             cleanup()
+#         elif sys.argv[1] == 'scrape':
+#             acc = load_accounts('data/accounts.txt')
+#             for x in range(len(acc)):
+#                 print('Account {}'.format(x+1))
+#                 scrape_from_user(acc, x)
+#         elif sys.argv[1] == 'save':
+#             tweets = load_tweets('data/tweet_ids.txt')
+#             print(tweets)
+#             with open('data/tweets.pickle', 'wb+') as f:
+#                 pickle.dump(tweets, f)
+#         elif sys.argv[1] == 'load':
+#             with open('data/tweets.pickle') as f:
+#                 tweets = pickle.load(f)
+#         else:
+#             print('Not a recognized argument')
